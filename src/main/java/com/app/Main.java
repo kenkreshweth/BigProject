b17ce421d952c7c45332eb640fa03a860cc57b7c
Shweth Kenkre <kenkreshweth@gmail.com>
2022-07-04 12:28:50 +0530

    Added Java demo application files

---
+++ src/main/java/com/app/Main.java
@@ -0,0 +1,14 @@
+package com.app;
+
+
+public class Main 
+{
+    public static void main( String[] args )
+    {
+        System.out.println(checkIfInputIsAnEvenNumber(122));
+        
+    }
+    public static boolean checkIfInputIsAnEvenNumber(int num) {
+    	return num%2==0;
+    }
+}
---
+++ src/test/java/com/app/TestMain.java
@@ -0,0 +1,10 @@
+package com.app;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+public class TestMain {
+	@Test
+	public void testInputIsEven() {
+		assertTrue(Main.checkIfInputIsAnEvenNumber(23));
+}
+}
